7c98b7069163cf0c743083aa61535674f0845889
Kevin Funez <kevin.funez315@gmail.com>
2021-09-10 09:22:55 -0700

    add dropdown to html

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
+$(document).ready(function() {
+  const exchange = $('#exchange').val();
+  let request = new XMLHttpRequest();
+  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  }
+  request.open("GET", img, true);
+  request.send();
+  $('.displayCurrency').html(`Currency Exchange Rate: ${response.conversion_rates[exchange]}`);
+})
