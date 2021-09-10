d54b28a07b16c0791e79795212a0f715a6dc71fc
Kevin Funez <kevin.funez315@gmail.com>
2021-09-10 11:44:34 -0700

    WIP making input value into string

--- src/main.js
+++ src/main.js
@@ -4,16 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-  const exchange = $('#exchange').val();
-  let request = new XMLHttpRequest();
-  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-  request.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      getElements(response);
+  $('#submit').click(function() {
+    let currency = $('#currency').val();
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+    console.log(currency);
+    request.open("GET", url, true);
+    request.send();
+    function getElements(response) {
+      $('.displayCurrency').html(`Currency Exchange Rate: ${response.conversion_rates[currency]}`);
     }
-  }
-  request.open("GET", img, true);
-  request.send();
-  $('.displayCurrency').html(`Currency Exchange Rate: ${response.conversion_rates[exchange]}`);
-})
+  });
+});
