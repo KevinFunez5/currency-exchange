75afcf9524a5b76bcb68aff194c75308c0b89c0b
Kevin Funez <kevin.funez315@gmail.com>
2021-09-13 18:34:35 -0700

    add complete readme

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+
+function checkAmount(input) {
+  if (input <= 0) {
+    return new Error("Not a valid Date! You IDIOT!!");
+  } else {
+    return true;
+  }
+}
+
 $(document).ready(function() {
   $('#submit').click(function() {
-    let currency = $('#currency').val();
-    let input = $('#dollar').val();
+    const currency = $('#currency').val();
+    const input = $('#dollar').val();
     parseInt(input);
     let request = new XMLHttpRequest();
     const url = `https://v6.exchangerate-api.com/v6/007304291feade653405d100/latest/USD`;
@@ -16,13 +25,27 @@ $(document).ready(function() {
         getElements(response);
       }
     };
-    console.log(currency);
-    console.log(input);
     request.open("GET", url, true);
     request.send();
     function getElements(response) {
       $('.displayRate').html(`Currency Exchange Rate: ${response.conversion_rates[currency]}`);
       $('.displayExchange').html(`Amount in ${currency}: ${response.conversion_rates[currency] * input}`);
     }
+    try {
+      const isinputValid = checkAmount(input);
+      if (isinputValid instanceof Error){
+        console.error(isinputValid.message);
+        throw RangeError("Not a valid Date! You IDIOT!!");
+      } else {
+        $("#displayDate").hide();
+        console.log("Try was successful, no need to catch. Not an idiot :)");
+      }
+    } catch(error){
+      console.error(`Red alert! There's an error! ${error.message}`);
+      $('#showError').text("please enter a valid amount >:(");
+      $('.displayRate').hide();
+      $('.displayExchange').hide();
+    }
   });
 });
+
